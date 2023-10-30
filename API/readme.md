3e33dbf578142087c792541ffcb5ff1b1a48e685
semsemihere <sh6042@nyu.edu>
2023-10-29 23:47:57 -0400

    updates on sample codes

--- API/readme.md
+++ API/readme.md
@@ -1,7 +1,14 @@
 This file deals will Wikipedia API
+document[https://wikipedia-api.readthedocs.io/_/downloads/en/latest/pdf/]
 
 installation
 `pip3 install wikipedia-api`
 
 import
 `import wikipediaapi`
+
+data extraction format 
+- extract_format=wikipediaapi.ExtractFormat.WIKI
+    - returns information in text Format
+- extract_format=wikipediaapi.ExtractFormat.HTML
+    - returns information in HTML Format
--- API/wiki.py
+++ API/wiki.py
@@ -2,9 +2,40 @@
 # Simply grabs wikipedia's python article & prints the first line
 
 import wikipediaapi
-wiki_wiki = wikipediaapi.Wikipedia('MyProject', 'en')
+wiki_wiki = wikipediaapi.Wikipedia(
+    user_agent = 'MyProject', 
+    language = 'en', 
+    extract_format = wikipediaapi.ExtractFormat.WIKI)
 
-page_py = wiki_wiki.page('Python_(programming_language)')
 
-print(page_py.title)
-print(page_py.summary[0:60])
+# grab page sections
+def print_sections(sections, level=0):
+    for s in sections:
+        print("%s: %s - %s" % ("*" * (level + 1), s.title, s.text[0:40]))
+        print_sections(s.sections, level + 1)
+
+# grab page in other language
+def print_langlinks(page):
+    langlinks = page.langlinks
+    for k in sorted(langlinks.keys()):
+        v = langlinks[k]
+        print("%s: %s - %s: %s" % (k, v.language, v.title, v.fullurl))
+
+
+def main():
+    page_py = wiki_wiki.page('Python_(programming_language)')
+
+    print("Title: ", page_py.title)
+    print("First line: ", page_py.summary[0:60])
+    print("Sections: ", page_py.text)
+
+    print_sections(page_py.sections)
+
+    print_langlinks(page_py)
+    page_py_cs = page_py.langlinks['ko'] # gets wiki in korean
+    print("Page - Summary: %s" % page_py_cs.summary[0:100])
+
+
+
+if __name__ == '__main__':
+    main()
