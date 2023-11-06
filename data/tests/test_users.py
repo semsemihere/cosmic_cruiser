b8e287b7c8271e252a188288cb5520f80c798096
George Gonzalez <geg321@nyu.edu>
2023-11-05 23:32:30 -0500

    adding create_user and its test case

--- data/tests/test_users.py
+++ data/tests/test_users.py
@@ -12,3 +12,9 @@ def test_get_users():
         assert isinstance(user, dict)
         assert usrs.LEVEL in user
         assert isinstance(user[usrs.LEVEL], int)
+
+def test_create_user():
+    user = usrs.create_user("test")
+    assert user=={"test": 0}
+    user2 = usrs.create_user("test")
+    assert user2==-1
--- data/users.py
+++ data/users.py
@@ -5,6 +5,15 @@ This module interfaces to our user data.
 LEVEL = 'level'
 MIN_USER_NAME_LEN = 2
 
+users = {
+        "Callahan": {
+            LEVEL: 0,
+        },
+        "Reddy": {
+            LEVEL: 1,
+        },
+    }
+
 
 def get_users():
     """
@@ -15,12 +24,12 @@ def get_users():
         - That dictionary must at least include a LEVEL member that has an int
         value.
     """
-    users = {
-        "Callahan": {
-            LEVEL: 0,
-        },
-        "Reddy": {
-            LEVEL: 1,
-        },
-    }
     return users
+
+
+def create_user(username):
+    if (username in users):
+        return -1
+    else:
+        users.update({username: 0})
+        return {username: 0}
