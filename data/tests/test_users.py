ebe7526700845921d95c32840cfffcad916429ae
George Gonzalez <geg321@nyu.edu>
2023-11-08 21:06:28 -0500

    Update test_users.py

--- data/tests/test_users.py
+++ data/tests/test_users.py
@@ -1,6 +1,6 @@
 import pytest
 import data.users as usrs
-
+from unittest.mock import patch
 
 def test_get_users():
     users = usrs.get_users()
@@ -24,8 +24,9 @@ def test_create_user_ideal():
     user2 = usrs.create_user("test")
     assert user2==-1
 
+@patch('data.users.create_user', side_effect=ValueError(), autospec=True)
 # Test to make sure the exception is handled 
-def test_create_user_fail():
+def test_create_user_fail(mockArg):
     with pytest.raises(ValueError):
         usrs.create_user("")
         
