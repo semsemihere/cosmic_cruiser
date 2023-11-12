86671ca89bcd82aa9ff40e1a882d9a8d3867248e
semsemihere <sh6042@nyu.edu>
2023-11-12 18:25:48 -0500

    users update
    functions added in users

--- data/tests/test_users.py
+++ data/tests/test_users.py
@@ -5,11 +5,11 @@ from unittest.mock import patch
 # fixture
 @pytest.fixture
 def check_user():
-    return usrs.get_users()
+    return usrs.get_all_users()
 
 
-def test_get_users(check_user):
-    # users = usrs.get_users()
+def test_get_all_users(check_user):
+    # users = usrs.get_all_users()
     
     assert isinstance(check_user, dict)
     assert len(check_user) > 0  # at least one user!
@@ -24,7 +24,7 @@ def test_create_user_ideal():
     username = usrs.create_user("test", "test@gmail.com", "Test", "Test", 1111111111)
 
     # username = create_test_user()
-    users = usrs.get_users()
+    users = usrs.get_all_users()
     assert (username in users)
 
 # @patch('data.users.create_user', side_effect=ValueError(), autospec=True)
@@ -44,7 +44,7 @@ def test_create_user_fail():
 def test_delete_user():
     # username = create_test_user()
     username = usrs.create_user('semsemi', 'sh6042@nyu.edu', 'Semi', 'Hong', 1987654321)
-    users = usrs.get_users()
+    users = usrs.get_all_users()
     usrs.delete_user(username)
     assert (not(username in users))
     # user2 = usrs.delete_user(username)
--- data/users.py
+++ data/users.py
@@ -1,7 +1,7 @@
 """
 This module interfaces to our user data.
 """
-# import random
+# import bcrypt
 
 LEVEL = 'level'
 MIN_USER_NAME_LEN = 2
@@ -21,6 +21,7 @@ Each users have ...
         - user
 -
 """
+
 users = {
         'ch123': {
             'email': 'cas123@nyu.edu',
@@ -51,41 +52,6 @@ users = {
     }
 
 
-def get_users():
-    """
-    Our contract:
-        - No arguments.
-        - Returns a dictionary of users keyed on user name (a str).
-        - Each user name must be the key for a dictionary.
-        - That dictionary must at least include a LEVEL member that has an int
-        value.
-    """
-    return users
-
-
-# def id_generator():
-#     return random.randint(10000, 99999)
-
-
-# def create_admin(username, email, first_name, last_name, phone):
-
-#     id = id_generator()
-
-#     if not username:
-#         raise ValueError()
-#     elif (username in users):
-#         return False
-#     else:
-#         users[id] = {
-#             'email': email,
-#             'username': username,
-#             'first_name': first_name,
-#             'last_name': last_name,
-#             'phone': phone,
-#             'role': 'admin'}
-#         return True
-
-
 def create_user(username, email, first_name, last_name, phone):
 
     # id = id_generator()
@@ -101,10 +67,62 @@ def create_user(username, email, first_name, last_name, phone):
             'first_name': first_name,
             'last_name': last_name,
             'phone': phone,
-            'role': 'user'}
+            'role': 'user'
+        }
         return username
 
+# Password Hashing
+# def password_encode(password):
+#     encoded_pwd = password.encode('utf-8')
+#     salt = bcrypt.gensalt()
+#     hashed_password = bcrypt.hashpw(encoded_pwd, salt)
 
+#     return hashed_password
+
+
+# Generates User ID
+# def id_generator():
+#     return random.randint(10000, 99999)
+
+
+def get_all_users():
+    """
+    Our contract:
+        - No arguments.
+        - Returns a dictionary of users keyed on user name (a str).
+        - Each user name must be the key for a dictionary.
+        - That dictionary must at least include a LEVEL member that has an int
+        value.
+    """
+    return users
+
+
+# Get specific user information
+def get_user(username):
+    if not (username in users):
+        return -1
+    else:
+        return users[username]
+
+
+# Update User information
+def update_user(username, field, update):
+    """
+    parameter: username
+        use the username to grab the user information
+    return: True is successful, else -1
+    """
+    user_info = get_user(username)
+
+    if user_info == -1:
+        return user_info
+
+    user_info[field] = update
+
+    return True
+
+
+# Remove the user from the db
 def delete_user(username):
     if (not (username in users)):
         return -1
