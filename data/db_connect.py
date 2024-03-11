1876ff2f81228715214e27c02d4fe8a3dc9f728c
George Gonzalez <geg321@nyu.edu>
2024-03-10 21:59:42 -0400

    Revert "testing pythonanywhere fix"
    This reverts commit a568cf3fb2934668c96f44cdceb303350bb92cbd.

--- data/db_connect.py
+++ data/db_connect.py
@@ -3,21 +3,12 @@ import os
 
 import pymongo as pm
 
-import ssl
-
 LOCAL = "0"
 CLOUD = "1"
 
 MONGO_DB = 'ccDB'
 
-client = pm.MongoClient((f'mongodb+srv://semihong:asdf1234'
-                                      + '@cosmiccrusier.3cyi8m1.mongodb.net/'
-                                      + '?retryWrites=true&w=majority'
-                                      + '&connectTimeoutMS=30000'
-                                      + '&socketTimeoutMS=None'
-                                      + '&socketKeepAlive=True'
-                                      + '&connect=False'
-                                      + '&maxPoolsize=1'), ssl_cert_reqs=ssl.CERT_NONE)
+client = None
 
 MONGO_ID = '_id'
 
