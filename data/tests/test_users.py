99ddb2988383a828c47340cce4e03f776e942675
semsemihere <sh6042@nyu.edu>
2024-05-07 23:07:15 -0400

    users test

--- data/tests/test_users.py
+++ data/tests/test_users.py
@@ -15,7 +15,8 @@ def create_test_user():
         usrs.delete_user(USERNAME)
         
     print(usrs.get_all_users())
-    id = usrs.create_user("test@gmail.com", USERNAME, PASSWORD, "Test", "Test", 1111111111, ROLE)
+    # id = usrs.create_user("test@gmail.com", USERNAME, PASSWORD, "Test", "Test", 1111111111, ROLE)
+    id = usrs.create_user("test@gmail.com", USERNAME, PASSWORD, "Test", "Test", 1111111111)
     if id:
         yield USERNAME
 
@@ -36,14 +37,17 @@ def test_create_user_ideal(create_test_user):
 
 
 def test_create_dup_user(create_test_user):
+    # with pytest.raises(ValueError):
+    #     usrs.create_user("test@gmail.com", USERNAME, PASSWORD, "Test", "Test", 1111111111, ROLE)
     with pytest.raises(ValueError):
-        usrs.create_user("test@gmail.com", USERNAME, PASSWORD, "Test", "Test", 1111111111, ROLE)
+        usrs.create_user("test@gmail.com", USERNAME, PASSWORD, "Test", "Test", 1111111111)
 
 
 # Test to make sure the exception is handled 
 def test_create_user_fail():
     with pytest.raises(ValueError):
-        usrs.create_user("", "","" , "", "", "", "")
+        # usrs.create_user("", "","" , "", "", "", "")
+        usrs.create_user("", "","" , "", "", "")
         
 def test_delete_user_ideal(create_test_user):
     userId = create_test_user
@@ -57,10 +61,12 @@ def test_delete_non_user(create_test_user):
     
 
 def test_login_user_ideal(create_test_user):
-    assert(usrs.login_user(create_test_user,PASSWORD, ROLE))
+    # assert(usrs.login_user(create_test_user,PASSWORD, ROLE))
+    assert(usrs.login_user(create_test_user,PASSWORD))
     
 def test_login_bad_user():
-    assert(not(usrs.login_user(BAD_USERNAME,BAD_PASSWORD, BAD_ROLE)))
+    # assert(not(usrs.login_user(BAD_USERNAME,BAD_PASSWORD, BAD_ROLE)))
+    assert(not(usrs.login_user(BAD_USERNAME,BAD_PASSWORD)))
     
 
 def test_exists(create_test_user):
@@ -78,4 +84,5 @@ def test_to_skip():
 
 def test_login_user(create_test_user):
     # password = "wrong_password"
-    assert not usrs.login_user(USERNAME, BAD_PASSWORD, ROLE)
+    # assert not usrs.login_user(USERNAME, BAD_PASSWORD, ROLE)
+    assert not usrs.login_user(USERNAME, BAD_PASSWORD)
