f8f5f7e7951ea5b94a914561e2a15e23f8c02d46
George Gonzalez <geg321@nyu.edu>
2024-05-07 23:39:03 -0400

    Update test_categories.py

--- data/tests/test_categories.py
+++ data/tests/test_categories.py
@@ -81,8 +81,8 @@ def test_generate_category_id():
 
 ADD_NAME = "New Category"
 
-def test_get_article_content():
-    assert None is not None
+# def test_get_article_content():
+#     assert None is not None
 
 # def test_add_article_no_category():
 #     assert categ.add_article_to_category("","") == False
